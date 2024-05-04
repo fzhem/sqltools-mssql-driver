5b1c7fddbec63a1d73769b6c19b643e18fb7b77e
fzhem <faizanessex@gmail.com>
2024-05-04 22:33:57 +0100

    Format with prettier

--- src/ls/driver.ts
+++ src/ls/driver.ts
@@ -1,13 +1,22 @@
-import { IResult, Binary } from 'mssql';
-import * as Queries from './queries';
-import AbstractDriver from '@sqltools/base-driver';
-import get from 'lodash/get';
-import { IConnectionDriver, NSDatabase, ContextValue, Arg0, MConnectionExplorer } from '@sqltools/types';
-import parse from './parser';
-import { v4 as generateId } from 'uuid';
-import reservedWordsCompletion from './reserved-words';
+import { IResult, Binary } from "mssql";
+import * as Queries from "./queries";
+import AbstractDriver from "@sqltools/base-driver";
+import get from "lodash/get";
+import {
+  IConnectionDriver,
+  NSDatabase,
+  ContextValue,
+  Arg0,
+  MConnectionExplorer,
+} from "@sqltools/types";
+import parse from "./parser";
+import { v4 as generateId } from "uuid";
+import reservedWordsCompletion from "./reserved-words";
 
-export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> implements IConnectionDriver {
+export default class MSSQL
+  extends AbstractDriver<MSSQLLib.ConnectionPool, any>
+  implements IConnectionDriver
+{
   queries = Queries;
 
   private retryCount = 0;
@@ -16,43 +25,50 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
       return this.connection;
     }
 
-    const { encrypt, ...mssqlOptions }: any = this.credentials.mssqlOptions || { encrypt: true };
+    const { encrypt, ...mssqlOptions }: any = this.credentials.mssqlOptions || {
+      encrypt: true,
+    };
 
-    let encryptAttempt = typeof encrypt !== 'undefined'
-      ? encrypt : true;
-    if (typeof encryptOverride !== 'undefined') {
+    let encryptAttempt = typeof encrypt !== "undefined" ? encrypt : true;
+    if (typeof encryptOverride !== "undefined") {
       encryptAttempt = encryptOverride;
     }
 
-    if (this.credentials.askForPassword && get(mssqlOptions, 'authentication.type') && get(mssqlOptions, 'authentication.options.userName')) {
-      mssqlOptions.authentication.options.password = mssqlOptions.authentication.options.password || this.credentials.password;
+    if (
+      this.credentials.askForPassword &&
+      get(mssqlOptions, "authentication.type") &&
+      get(mssqlOptions, "authentication.options.userName")
+    ) {
+      mssqlOptions.authentication.options.password =
+        mssqlOptions.authentication.options.password ||
+        this.credentials.password;
       this.credentials.password = null;
     }
 
-
-    const pool = new MSSQLLib.ConnectionPool(this.credentials.connectString || {
-      database: this.credentials.database,
-      connectionTimeout: this.credentials.connectionTimeout * 1000,
-      server: this.credentials.server,
-      user: this.credentials.username,
-      password: this.credentials.password,
-      domain: this.credentials.domain || undefined,
-      port: this.credentials.port,
-      ...mssqlOptions,
-      options: {
-        ...((mssqlOptions || {}).options || {}),
-        encrypt: encryptAttempt,
-      },
-    });
+    const pool = new MSSQLLib.ConnectionPool(
+      this.credentials.connectString || {
+        database: this.credentials.database,
+        connectionTimeout: this.credentials.connectionTimeout * 1000,
+        server: this.credentials.server,
+        user: this.credentials.username,
+        password: this.credentials.password,
+        domain: this.credentials.domain || undefined,
+        port: this.credentials.port,
+        ...mssqlOptions,
+        options: {
+          ...((mssqlOptions || {}).options || {}),
+          encrypt: encryptAttempt,
+        },
+      }
+    );
 
     await new Promise((resolve, reject) => {
-      pool.on('error', reject);
+      pool.on("error", reject);
       pool.connect().then(resolve).catch(reject);
-    }).catch(e => {
+    }).catch((e) => {
       if (this.retryCount === 0) {
         this.retryCount++;
-        return this.open(!encryptAttempt)
-        .catch(() => {
+        return this.open(!encryptAttempt).catch(() => {
           this.retryCount = 0;
           return Promise.reject(e);
         });
@@ -73,14 +89,27 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
     this.connection = null;
   }
 
-  public query: (typeof AbstractDriver)['prototype']['query'] = async (originalQuery, opt = {}) => {
+  public query: (typeof AbstractDriver)["prototype"]["query"] = async (
+    originalQuery,
+    opt = {}
+  ) => {
     const pool = await this.open();
     const { requestId } = opt;
     const request = pool.request();
     request.multiple = true;
-    const query = originalQuery.toString().replace(/^[ \t]*GO;?[ \t]*$/gmi, '');
-    const { recordsets = [], rowsAffected, error } = <IResult<any> & { error: any }>(await request.query(query).catch(error => Promise.resolve({ error, recordsets: [], rowsAffected: [] })));
-    const queries = parse(query, 'mssql');
+    const query = originalQuery.toString().replace(/^[ \t]*GO;?[ \t]*$/gim, "");
+    const {
+      recordsets = [],
+      rowsAffected,
+      error,
+    } = <IResult<any> & { error: any }>(
+      await request
+        .query(query)
+        .catch((error) =>
+          Promise.resolve({ error, recordsets: [], rowsAffected: [] })
+        )
+    );
+    const queries = parse(query, "mssql");
     return queries.map((q, i): NSDatabase.IResult => {
       const r = recordsets[i] || [];
       const columnNames = [];
@@ -90,13 +119,15 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
         if (col && col.type && col.type.name === Binary.name) {
           bufferCols.push(col.name);
         }
-      })
+      });
       const messages = [];
       if (error) {
         messages.push(this.prepareMessage(error.message || error.toString()));
       }
-      if (typeof rowsAffected[i] === 'number')
-        messages.push(this.prepareMessage(`${rowsAffected[i]} rows were affected.`));
+      if (typeof rowsAffected[i] === "number")
+        messages.push(
+          this.prepareMessage(`${rowsAffected[i]} rows were affected.`)
+        );
 
       return {
         requestId,
@@ -106,19 +137,26 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
         messages,
         error,
         query: q,
-        results: Array.isArray(r) ? r.map((row) => {
-          bufferCols.forEach(c => {
-            try {
-              row[c] = `0x${Buffer.from(row[c]).toString('hex').toUpperCase()}`
-            } catch (_ee) {}
-          })
-          return row;
-        }) : [],
+        results: Array.isArray(r)
+          ? r.map((row) => {
+              bufferCols.forEach((c) => {
+                try {
+                  row[c] = `0x${Buffer.from(row[c])
+                    .toString("hex")
+                    .toUpperCase()}`;
+                } catch (_ee) {}
+              });
+              return row;
+            })
+          : [],
       };
-    })
-  }
+    });
+  };
 
-  public async getChildrenForItem({ item, parent }: Arg0<IConnectionDriver['getChildrenForItem']>) {
+  public async getChildrenForItem({
+    item,
+    parent,
+  }: Arg0<IConnectionDriver["getChildrenForItem"]>) {
     switch (item.type) {
       case ContextValue.CONNECTION:
       case ContextValue.CONNECTED_CONNECTION:
@@ -128,14 +166,29 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
         return this.getColumns(item as NSDatabase.ITable);
       case ContextValue.DATABASE:
         return <MConnectionExplorer.IChildItem[]>[
-          { label: 'Schemas', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.SCHEMA },
+          {
+            label: "Schemas",
+            type: ContextValue.RESOURCE_GROUP,
+            iconId: "folder",
+            childType: ContextValue.SCHEMA,
+          },
         ];
       case ContextValue.RESOURCE_GROUP:
         return this.getChildrenForGroup({ item, parent });
       case ContextValue.SCHEMA:
         return <MConnectionExplorer.IChildItem[]>[
-          { label: 'Tables', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.TABLE },
-          { label: 'Views', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.VIEW },
+          {
+            label: "Tables",
+            type: ContextValue.RESOURCE_GROUP,
+            iconId: "folder",
+            childType: ContextValue.TABLE,
+          },
+          {
+            label: "Views",
+            type: ContextValue.RESOURCE_GROUP,
+            iconId: "folder",
+            childType: ContextValue.VIEW,
+          },
           // { label: 'Functions', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.FUNCTION },
         ];
     }
@@ -143,50 +196,69 @@ export default class MSSQL extends AbstractDriver<MSSQLLib.ConnectionPool, any> 
   }
 
   public showRecords(table, opt) {
-    return this.searchItems(ContextValue.COLUMN, '', { tables: [table], limit: 1 })
-    .then(col => {
+    return this.searchItems(ContextValue.COLUMN, "", {
+      tables: [table],
+      limit: 1,
+    }).then((col) => {
       opt.orderCol = col[0].label;
       return super.showRecords(table, opt);
     });
   }
-  private async getChildrenForGroup({ parent, item }: Arg0<IConnectionDriver['getChildrenForItem']>) {
+  private async getChildrenForGroup({
+    parent,
+    item,
+  }: Arg0<IConnectionDriver["getChildrenForItem"]>) {
     switch (item.childType) {
       case ContextValue.SCHEMA:
-        return this.queryResults(this.queries.fetchSchemas(parent as NSDatabase.IDatabase));
+        return this.queryResults(
+          this.queries.fetchSchemas(parent as NSDatabase.IDatabase)
+        );
       case ContextValue.TABLE:
-        return this.queryResults(this.queries.fetchTables(parent as NSDatabase.ISchema));
+        return this.queryResults(
+          this.queries.fetchTables(parent as NSDatabase.ISchema)
+        );
       case ContextValue.VIEW:
-        return this.queryResults(this.queries.fetchViews(parent as NSDatabase.ISchema));
+        return this.queryResults(
+          this.queries.fetchViews(parent as NSDatabase.ISchema)
+        );
       case ContextValue.FUNCTION:
         return []; //this.queryResults(this.queries.fetchFunctions(parent as NSDatabase.ISchema));
     }
     return [];
   }
 
-  private async getColumns(parent: NSDatabase.ITable): Promise<NSDatabase.IColumn[]> {
+  private async getColumns(
+    parent: NSDatabase.ITable
+  ): Promise<NSDatabase.IColumn[]> {
     const results = await this.queryResults(this.queries.fetchColumns(parent));
-    return results.map(col => ({
+    return results.map((col) => ({
       ...col,
-      iconName: col.isPk ? 'pk' : (col.isFk ? 'fk' : null),
+      iconName: col.isPk ? "pk" : col.isFk ? "fk" : null,
       childType: ContextValue.NO_CHILD,
-      table: parent
+      table: parent,
     }));
   }
 
-  public searchItems(itemType: ContextValue, search: string, extraParams: any = {}): Promise<NSDatabase.SearchableItem[]> {
+  public searchItems(
+    itemType: ContextValue,
+    search: string,
+    extraParams: any = {}
+  ): Promise<NSDatabase.SearchableItem[]> {
     switch (itemType) {
       case ContextValue.TABLE:
         return this.queryResults(this.queries.searchTables({ search }));
       case ContextValue.COLUMN:
-        return this.queryResults(this.queries.searchColumns({ search, ...extraParams }));
+        return this.queryResults(
+          this.queries.searchColumns({ search, ...extraParams })
+        );
     }
   }
 
   public getStaticCompletions = async () => {
     return reservedWordsCompletion;
-  }
+  };
 
-    // public getColumns(): Promise<NSDatabase.IColumn[]> {
+  // public getColumns(): Promise<NSDatabase.IColumn[]> {
   //   return this.query(this.queries.fetchColumns)
   //     .then(([queryRes]) => {
   //       return queryRes.results

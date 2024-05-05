4f15c0b66ea624b5687b953ca3c9c2621ba68d16
Hemotra, Faizan <Faizan.Hemotra@uhs.nhs.uk>
2024-05-05 12:54:32 +0100

    Add type for driver

--- src/ls/plugin.ts
+++ src/ls/plugin.ts
@@ -1,11 +1,11 @@
-import { ILanguageServerPlugin } from '@sqltools/types';
+import { ILanguageServerPlugin, IConnectionDriverConstructor } from '@sqltools/types';
 import MSSQL from './driver';
 import { DRIVER_ALIASES } from './../constants';
 
 const MSSQLDriverPlugin: ILanguageServerPlugin = {
   register(server) {
     DRIVER_ALIASES.forEach(({ value }) => {
-      server.getContext().drivers.set(value, MSSQL);
+      server.getContext().drivers.set(value, MSSQL as IConnectionDriverConstructor);
     });
   }
 }
